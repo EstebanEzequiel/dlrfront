661939e95b59e70934e5ac2eb82671f01088d298
estebanezequiel <estebanezequielceballos@gmail.com>
2020-12-09 02:48:19 -0300

    estructura de panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
+import { TablaComponent } from './panel/tabla/tabla.component';
+import { FormularioComponent } from './panel/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    TablaComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
