9ca10d7b5b08d48f8de3fc3c3b0522c1e551690e
esteban ezequiel <estebanezequielceballos@gmail.com>
2020-12-16 13:37:11 -0300

    correcciones

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -7,24 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  dataField:Endpoint[]
+
+  constructor(){
+    this.dataField = [
+      {endpoint:'', method:'', security:'', table:'', properties:[] }
+    ]
+   }
 
   editField: string;
-  personList: Array<any> = [
-    { id: 1, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
-    { id: 2, name: 'Guerra Cortez', age: 45, companyName: 'Insectus', country: 'USA', city: 'San Francisco' },
-    { id: 3, name: 'Guadalupe House', age: 26, companyName: 'Isotronic', country: 'Germany', city: 'Frankfurt am Main' },
-    { id: 4, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
-    { id: 5, name: 'Elisa Gallagher', age: 31, companyName: 'Portica', country: 'United Kingdom', city: 'London' },
-  ];
-
-  awaitingPersonList: Array<any> = [
-    { id: 6, name: 'George Vega', age: 28, companyName: 'Classical', country: 'Russia', city: 'Moscow' },
-    { id: 7, name: 'Mike Low', age: 22, companyName: 'Lou', country: 'USA', city: 'Los Angeles' },
-    { id: 8, name: 'John Derp', age: 36, companyName: 'Derping', country: 'USA', city: 'Chicago' },
-    { id: 9, name: 'Anastasia John', age: 21, companyName: 'Ajo', country: 'Brazil', city: 'Rio' },
-    { id: 10, name: 'John Maklowicz', age: 36, companyName: 'Mako', country: 'Poland', city: 'Bialystok' },
-  ];
 
   updateList(id: number, property: string, event: any) {
     const editField = event.target.textContent;
@@ -37,7 +28,7 @@ export class TableComponent implements OnInit {
   }
 
   add() {
-    if (this.awaitingPersonList.length > 0) {
+    if (this.awaitingPersonList.length >= 0) {
       const person = this.awaitingPersonList[0];
       this.personList.push(person);
       this.awaitingPersonList.splice(0, 1);
@@ -45,10 +36,18 @@ export class TableComponent implements OnInit {
   }
 
   changeValue(id: number, property: string, event: any) {
-    this.editField = event.target.textContent;
+    this.editField = event.target.value;
   }
 
   ngOnInit(): void {
   }
   
+}
+
+interface Endpoint{
+   endpoint: string, method: string, security: string, table: string, properties: EndpointProperties[]  
+}
+
+interface EndpointProperties{
+  endpoint: string, method: string, properties: string, column: string, require: string, type: string  
 }
